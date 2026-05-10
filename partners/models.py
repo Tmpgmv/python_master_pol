fe4cc88fde7b0cb4a05906c9473fdd29a8c88be4
Your Name <you@example.com>
2026-05-10 19:10:46 +0300

    016

--- partners/models.py
+++ partners/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Partner(models.Model):
     CHOICES = (("ЗАО", "ЗАО"), ("ОАО", "ОАО"), ("ООО", "ООО"), ("ПАО", "ПАО"))
     type = models.CharField(max_length=50,
@@ -13,7 +14,6 @@ class Partner(models.Model):
     taxpayer_number = models.PositiveBigIntegerField(verbose_name="ИНН")
     rating = models.PositiveIntegerField(verbose_name="Рейтинг")
 
-
     def get_phone_number(self):
         formatted = "+{} {} {} {} {} {}".format(str(self.phone_number)[0],
                                                 str(self.phone_number)[1:4],
@@ -23,9 +23,27 @@ class Partner(models.Model):
                                                 str(self.phone_number)[12:])
         return formatted
 
+    def total_sale(self):
+        total = 0
+
+        for pp in self.partnerproduct_set.all():
+            total += pp.amount
+
+        return total
+
+    def get_discount(self):
+        discount = 0
+        if (self.total_sale() >= 10000 and self.total_sale() < 50000):
+            discount = 5
+        elif (self.total_sale() >= 50000 and self.total_sale() < 300000):
+            discount = 10
+        elif (self.total_sale() >= 300000):
+            discount = 15
+
+        return f"{discount} %"
+
     def __str__(self):
         return self.name
-
 
     class Meta:
         ordering = ["name"]
