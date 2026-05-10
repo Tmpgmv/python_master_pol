1746e1b236c72f28f2b5830380ea7dbd09f321d8
Your Name <you@example.com>
2026-05-10 19:51:46 +0300

    019

--- partners/views.py
+++ partners/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from partners.models import Partner
+
+
+class PartnerCreateView(SuccessMessageMixin,
+                        CreateView):
+    model = Partner
+    fields = "__all__"
+    success_url = reverse_lazy("home")
+    success_message = "Партнер добавлен."
