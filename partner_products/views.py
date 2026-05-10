61df03a10809b1c12a354ea5ae74ac2ed1191252
Your Name <you@example.com>
2026-05-10 20:57:27 +0300

    028

--- partner_products/views.py
+++ partner_products/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from partner_products.models import PartnerProduct
+
+
+class SaleHistoryView(ListView):
+    model = PartnerProduct
