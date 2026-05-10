67ca6d2c9d593db8523667548d35ab187c95a1d0
Your Name <you@example.com>
2026-05-10 22:51:05 +0300

    039

--- general/service.py
+++ general/service.py
@@ -1,5 +1,7 @@
 import math
 
+from general.exceptions import InvalidParamsException
+
 
 class MaterialCalculator():
     def calculate_material(self,
@@ -20,6 +22,38 @@ class MaterialCalculator():
         material_amount = 0 # количество необходимого материала с учетом возможного брака материала.
 
 
+        try:
+            self.validate_params(produced_product_amount,
+                                 param_1,
+                                 param_2)
+
+        except (InvalidParamsException) as e:
+            print(e)
+            return -1
+
+
         material_amount = math.ceil(material_amount)
         assert material_amount >= 0, "Количество материала не может быть отрицательным" # PKGH Отладка.
         return material_amount
+
+    def validate_params(self,
+                        produced_product_amount,
+                        param_1,
+                        param_2):
+        self.validate_produced_product_amount(produced_product_amount)
+        self.validate_param(param_1, "param_1")
+        self.validate_param(param_2, "param_2")
+
+    def validate_produced_product_amount(self, produced_product_amount):
+        if not isinstance(produced_product_amount, int):
+            raise InvalidParamsException("produced_product_amount должено быть быть целым числом")
+
+        if produced_product_amount <= 0:
+            raise InvalidParamsException("produced_product_amount должено быть положительным числом")
+
+    def validate_param(self, param, param_name):
+        if not isinstance(param, (int, float)):
+            raise InvalidParamsException(f"{param_name} должно быть вещественным числом")
+
+        if param <= 0:
+            raise InvalidParamsException(f"{param_name} должено быть положительным числом")
