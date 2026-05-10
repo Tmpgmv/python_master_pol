f2fe41613f05daa5d9d769b74ed85711091e8930
Your Name <you@example.com>
2026-05-10 22:15:56 +0300

    031

--- partner_products/views.py
+++ partner_products/views.py
@@ -5,3 +5,7 @@ from partner_products.models import PartnerProduct
 
 class SaleHistoryView(ListView):
     model = PartnerProduct
+
+    def get_queryset(self):
+        qs = super().get_queryset().filter(partner__pk=self.kwargs["pk"])
+        return qs
