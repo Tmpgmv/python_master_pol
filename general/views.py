a700b6c6aa7aeea1808756014e08c321cfcb9bb3
Your Name <you@example.com>
2026-05-10 23:17:18 +0300

    041

--- general/views.py
+++ general/views.py
@@ -1,5 +1,18 @@
 from django.views.generic import TemplateView
 
+from general.service import MaterialCalculator
+from partner_products.models import PartnerProduct
+
 
 class CalculateMaterialView(TemplateView):
     template_name = 'general/calculate.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        material_calculator = MaterialCalculator()
+        context["amount"] = material_calculator.calculate_material(product_type_id=1,
+                                               mateirial_type_id=1,
+                                               produced_product_amount=1,
+                                               param_1=1,
+                                               param_2=1)
+        return context
